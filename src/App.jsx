6c64ba61df5fc04bec762fccc0756a5a44ba70cd
lzy6330 <nenu_lzy@qq.com>
2026-04-04 07:20:41 +0800

    大括号语法识别字符串，变量，函数，方法，使用js对象

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+const count = 100
+
+function getName(){
+  return 'Jack'
+}
+
 function App() {
   return (
     <div className="App">
       你好啊🤔!!
+      {/**使用引号传递字符串 */}
+      {'this is 引号传递的字符串'}
+      {/**识别变量 */}
+      {count}
+      {/**函数调用 */}
+      {getName()}
+      {/**方法调用 */}
+      {new Date().getDate()}
+      {/**使用js对象 */}
+      <div style={{color: 'red'}}> 一个div </div>
     </div>
   )
 }
