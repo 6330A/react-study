05b59c174664ed79e6c61ed1b1c5cc0ba9f67956
lzy6330 <nenu_lzy@qq.com>
2026-04-14 17:47:57 +0800

    useState基础使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 
-// 传统定义
-function Button1() {
-  return <button>click me button1!</button>
-}
+// useState实现计数器按钮
+import { useState } from 'react'
 
-// 箭头函数
-const Button2 = () => {
-  return <button>click me button2!</button>
-}
 
 function App() {
 
+  // 1.调用useState添加状态变量
+  const [count, setMyCount] = useState(0)
+
+  // 2.点击事件回调
+  const handleClick = () => {
+    setMyCount(count + 1)
+  }
+
   return (
     <div className="App">
-      {/**2.使用组件（渲染组件） */}
-      {/**自闭和 */}
-      <Button1 />
-      {/**成对标签 */}
-      <Button2></Button2>
-
+      <button onClick={handleClick}>{count}</button>
     </div>
   )
 }
