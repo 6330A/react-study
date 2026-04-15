ffc651d9b964a282b3cb05293deacb9e880724d4
lzy6330 <nenu_lzy@qq.com>
2026-04-15 11:03:54 +0800

    组件属性传递，父传子

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
+
+function Son(props){
+  // 3. 子组件通过props接收父组件传递的属性，并使用
+  return <div>son, {props.appName}</div>
+}
 
 function App() {
 
-  // 1. useRef 生成ref对象， 绑定到dom标签上
-  // 2. 通过ref对象.current获取dom元素
-  const inputRef = useRef(null)
-  const showDom = () => {
-    console.dir(inputRef.current)
-  }
+  // 1.父组件App定义属性，传递给子组件Son
+  const appName = 'App Name'
+
   return (
     <div>
-      <input type="text" ref={inputRef}></input>
-      <button onClick={showDom}>click me</button>
+      {/* 2. 子组件接收父组件传递的属性 */}
+      <Son appName={appName}/>
     </div>
   )
 }
