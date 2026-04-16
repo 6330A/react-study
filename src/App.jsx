8445fefdd71d87089762f32a3963929c7255f313
lzy6330 <nenu_lzy@qq.com>
2026-04-16 16:03:03 +0800

    useEffect使用，渲染完成后发送请求获取数据并再渲染，无用户事件参与

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
-import { useContext, createContext } from 'react'
-
-const MsgContext = createContext()
-
-// APP -> B -> A
-function A() {
-
-  const msg = useContext(MsgContext)
-  return (
-    <div>
-      this is A component {msg}
-    </div>
-  )
-}
-
-function B() {
-
-  return (
-    <div>
-      this is B component
-      <A />
-    </div>
-  )
-}
-
+import { useEffect, useState } from 'react'
+const URL = 'http://geek.itheima.net/v1_0/channels'
 function App() {
-
+  // 创建一个状态数据
+  const [list, setList] = useState([])
+  useEffect(() => {
+    async function getList() {
+      const res = await fetch(URL)
+      const jsonRes = await res.json()
+      console.log(jsonRes)
+      setList(jsonRes.data.channels)
+    }
+    getList()
+  }, [])
+  // 通过val属性绑定react状态
+  // 绑定onChange事件，通过事件参数e拿到输入框最新的值，反向修改到react状态
   return (
     <div>
-      <MsgContext.Provider value={'msg from app 跨层传递'}>
-        this App component
-        <B />
-      </MsgContext.Provider>
+      this is app
+      <ul>
+        {list.map(item => <li key = {item.id}>{item.name}</li>)}
+      </ul>
     </div>
   )
 }
-
 export default App
