a291848ecf35c5d38564ffcbdabe1a8387b705dd
lzy6330 <nenu_lzy@qq.com>
2026-04-16 16:00:48 +0800

    createContext, provider,useContext进行跨层传递

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import { useState } from 'react'
+import { useContext, createContext } from 'react'
 
-function A({getAName}) {
+const MsgContext = createContext()
 
+// APP -> B -> A
+function A() {
+
+  const msg = useContext(MsgContext)
   return (
     <div>
-      <p>组件A</p>
-      <button onClick={() => getAName('组件A张三')}>获取组件A的名字</button>
+      this is A component {msg}
     </div>
   )
 }
 
-function B({name}) {
+function B() {
 
   return (
     <div>
-      <p>组件B</p>
-      <p>组件A的名字是：{name}</p>
+      this is B component
+      <A />
     </div>
   )
 }
 
 function App() {
 
-  const [name, setName] = useState('')
-
-  const getAName = (name) => {
-    console.log(name)
-    setName(name)
-  }
-
   return (
     <div>
-      <A getAName={getAName}/>
-      <B name={name} />
+      <MsgContext.Provider value={'msg from app 跨层传递'}>
+        this App component
+        <B />
+      </MsgContext.Provider>
     </div>
   )
 }
